78dc51062b1eb464797a28c82d567ee53c719dde
Nishant <nishantjambhulkar6@gmail.com>
2024-03-23 20:57:18 +0530

    todays-classes 23March2024

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-console.log("hello diksha");
+// const HTTP = require("http");
+
+// let SERVER=HTTP.createServer();
+// SERVER.listen(5000,()=>{console.log('server is running')});
+
+
+const express=require('express');
+const app = express();
+
+// app.get('/', function (req, res) {
+//   res.send('Hello World')
+// })
+
+let PORT=3000;
+app.listen(PORT,()=>{
+    console.log(`Server is Running on  port ${PORT}`);
+});
+
+app.get( '/get' , (req,res) => {
+    console.log("hello diksha");
+    res.send({name:'Diksha'});
+});
